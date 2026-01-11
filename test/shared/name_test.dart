48c4207cc52e5a37016371a78869e5182b0aeb02
johnataa <johnata05@gmail.com>
2026-01-11 20:39:41 +0000

    Update Name unit tests to use testMany

--- test/shared/name_test.dart
+++ test/shared/name_test.dart
@@ -2,39 +2,67 @@ import 'package:finances/shared/value_objects/name/errors.dart';
 import 'package:finances/shared/value_objects/name/name.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../test_many.dart';
 import 'error_code_test.dart';
 
 void main() {
+  const minLength = Name.minLength;
+  const maxLength = Name.maxLength;
+
   group("Invalid name length", () {
-    test(
-      "Should be an invalid name when name length is less than ${Name.minLength}",
-      () {
-        // Arrange
-        String value = "A" * (Name.minLength - 1);
+    test("Should be an invalid name when name length is less than $minLength.", () {
+      // Arrange
+      int length = minLength - 1;
 
+      // Act
+      Name name = Name('A' * length);
+
+      // Assert
+      expect(name.hasError, true);
+      expect(name.value.length, lessThanOrEqualTo(minLength));
+      expect(name.error, TypeMatcher<InvalidNameLengthError>());
+    });
+
+    test("Should be an invalid name when name length is more than $maxLength.", () {
+      // Arrange
+      int length = maxLength + 1;
+
+      // Act
+      Name name = Name('A' * length);
+
+      // Assert
+      expect(name.hasError, true);
+      expect(name.value.length, greaterThanOrEqualTo(maxLength));
+      expect(name.error, TypeMatcher<InvalidNameLengthError>());
+    });
+  });
+
+  group("Valid name length", () {
+    testMany(
+      "Should be valid when name length is greater than or equal to $minLength.",
+      params: [minLength, minLength + 1],
+      test: (length) {
         // Act
-        Name result = Name(value);
+        Name name = Name("A" * length);
 
-        // Asserts
-        expect(result.hasError, true);
-        expect(result.value, value);
-        expect(result.error, TypeMatcher<InvalidNameLengthError>());
+        // Assert
+        expect(name.hasError, false);
+        expect(name.value.length, greaterThanOrEqualTo(minLength));
+        expect(() => name.error, throwsA(TypeMatcher<TypeError>()));
       },
     );
 
-    test(
-      "Should be an invalid name when name length is more than ${Name.maxLength}",
-      () {
-        // Arrange
-        String value = "A" * (Name.maxLength + 1);
-
+    testMany(
+      "Should be valid when name length is lesser than or equal to $maxLength.",
+      params: [maxLength - 1, maxLength],
+      test: (length) {
         // Act
-        Name result = Name(value);
+        Name name = Name("A" * length);
 
-        // Asserts
-        expect(result.hasError, true);
-        expect(result.value, value);
-        expect(result.error, TypeMatcher<InvalidNameLengthError>());
+        // Assert
+        expect(name.hasError, false);
+        expect(name.value.length, lessThanOrEqualTo(maxLength));
+        expect(() => name.error, throwsA(TypeMatcher<TypeError>()));
       },
     );
   });
